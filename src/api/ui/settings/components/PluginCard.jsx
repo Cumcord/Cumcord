9e69b314e5833696c3ac6adfb37146964ef66eb4
creatable <creatableog@protonmail.com>
2021-08-07 01:22:33 -0400

    Use functional component instead

--- src/api/ui/settings/components/PluginCard.jsx
+++ src/api/ui/settings/components/PluginCard.jsx
@@ -1,6 +1,7 @@
 import { React } from "commonModules";
 import webpackModules from "webpackModules";
 
+const useState = React.useState;
 const Card = webpackModules.findByDisplayName("Card");
 const Header = webpackModules.findByDisplayName("Header");
 const Text = webpackModules.findByDisplayName("Text");
@@ -10,48 +11,36 @@ const Switch = webpackModules.findByDisplayName("Switch");
 const { Separator } = webpackModules.findByProps("Separator");
 
 
-export default class PluginCard extends React.Component {
-  constructor(props) {
-    super(props);
+export default (props) => {
+  const plugin = window.cumcord.plugins.pluginCache[props.pluginId];
+  const [enabled, setEnabled] = useState(plugin.enabled);
 
-    const plugin = window.cumcord.plugins.pluginCache[props.pluginId];
-    this.state = {
-      plugin,
-      enabled: plugin.enabled,
-    }
-  }
-
-  render() {
-    const { plugin } = this.state;
-
-    return (
-      <Card
-        className="cumcord-plugin-card"
-        type="cardPrimary"
-        outline={false}
-        editable={false}
-      >
-        <Flex justify={Flex.Justify.BETWEEN} align={Flex.Align.CENTER}>
-          <div className="cumcord-card-header">
-            <Header className="cumcord-card-title">{plugin.manifest.name}</Header>
-            <Text className="cumcord-card-author" tag="h5">
-              {" "}
-              by <strong>{plugin.manifest.author}</strong>
-            </Text>
-          </div>
-          <Switch checked={this.state.enabled} onChange={
-              () => {
-                try {
-                  window.cumcord.plugins.togglePlugin(this.props.pluginId);
-                  // Unnecessary?
-                  this.setState({ enabled: plugin.enabled });
-                } catch { }
-              }
-            } />
-        </Flex>
-        {/* <Separator margin="10px" /> */}
-        <Markdown>{plugin.manifest.description}</Markdown>
-      </Card>
-    )
-  }
+  return (
+    <Card
+      className="cumcord-plugin-card"
+      type="cardPrimary"
+      outline={false}
+      editable={false}
+    >
+      <Flex justify={Flex.Justify.BETWEEN} align={Flex.Align.CENTER}>
+        <div className="cumcord-card-header">
+          <Header className="cumcord-card-title">{plugin.manifest.name}</Header>
+          <Text className="cumcord-card-author" tag="h5">
+            {" "}
+            by <strong>{plugin.manifest.author}</strong>
+          </Text>
+        </div>
+        <Switch checked={enabled} onChange={
+            () => {
+              try {
+                window.cumcord.plugins.togglePlugin(props.pluginId);
+                setEnabled(plugin.enabled);
+              } catch { }
+            }
+          } />
+      </Flex>
+      {/* <Separator margin="10px" /> */}
+      <Markdown>{plugin.manifest.description}</Markdown>
+    </Card>
+  )
 };
