2cc9547efd0f2e408fef7b1df257ace9ab53cd88
creatable <creatableog@protonmail.com>
2021-09-05 03:22:24 -0400

    Add websocket based plugin installation

--- src/api/websocket/commandHandler.js
+++ src/api/websocket/commandHandler.js
@@ -1,6 +1,8 @@
-import { loadPluginDev, devModeOn } from "devmode";
-
 // This file is a basic websocket command handler.
+
+import { loadPluginDev, devModeOn } from "devmode";
+import { importPlugin } from "plugins";
+import { showConfirmationModal } from "modals";
 
 export default function commandHandler(msg, ws) {
   var parsed;
@@ -17,6 +19,48 @@ export default function commandHandler(msg, ws) {
         "uuid": parsed["uuid"] || Math.random(),
         "status": "OK",
       }))
+      return;
+    case "install_plugin":
+      if (parsed["url"]) {
+        // Check if the URL is a valid URL.
+        if (!parsed["url"].match(/^(http|https):\/\/[^ "]+$/)) {
+          ws.send(JSON.stringify({
+            "name": "CUMCORD_WEBSOCKET",
+            "uuid": parsed["uuid"] || Math.random(),
+            "status": "ERROR",
+            "error": "Invalid URL.",
+          }))
+          return;
+        }
+
+        if (window["DiscordNative"]) {
+          // Focus the window to make sure the modal is visible.
+          DiscordNative.window.focus();
+        }
+
+        showConfirmationModal({header: "Do you want to install this plugin?", content: `Cumcord plugins can run code on your computer and can be potentially dangerous. Only click confirm if you trust the plugin from \`${parsed["url"]}\`.`, confirmText: "Install", cancelText: "Cancel", type: "danger"}, (result) => {
+          if (result) {
+            importPlugin(parsed["url"]);
+            return;
+          } else {
+            ws.send(JSON.stringify({
+              "name": "CUMCORD_WEBSOCKET",
+              "uuid": parsed["uuid"] || Math.random(),
+              "status": "ERROR",
+              "error": "Plugin installation cancelled.",
+            }))
+            return;
+          }
+        })
+      } else {
+        ws.send(JSON.stringify({
+          "uuid": parsed["uuid"] || Math.random(),
+          "status": "ERROR",
+          "error": "No URL provided.",
+        }))
+        // Unnecessary but cleaner.
+        return;
+      }
       return;
     case "install_plugin_dev":
       if (devModeOn) {
