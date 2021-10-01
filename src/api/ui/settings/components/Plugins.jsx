bb900e89401da39a2ed892bd329609b8354414c4
creatable <creatableog@protonmail.com>
2021-10-01 17:49:42 -0400

    Add error boundary to settings page

--- src/api/ui/settings/components/Plugins.jsx
+++ src/api/ui/settings/components/Plugins.jsx
@@ -1,6 +1,7 @@
 import webpackModules from "webpackModules";
 import { useNest } from "utils";
 import PluginCard from "./PluginCard.jsx";
+import { ErrorBoundary } from "components";
 import * as plugins from "plugins";
 
 const useState = React.useState;
@@ -22,33 +23,33 @@ export default () => {
   useNest(plugins.pluginCache);
 
   return (
-    <FormSection>
-      <FormTitle tag="h1">Plugins</FormTitle>
-      <Flex basis="auto" grow={1} shrink={1}>
-        <TextInput
-          className="cumcord-plugin-import"
-          placeholder="https://example.com/plugin"
-          type="text"
-          value={input}
-          onChange={e => setInput(e)}
-        ></TextInput>
-        <Button color={Button.Colors.BRAND} size={Button.Sizes.MEDIUM} onClick={
-          () => {
-            setInput("");
-            plugins.importPlugin(input);
-          }
-        }>
-          Add plugin
-        </Button>
-      </Flex>
-      <FormDivider className="cumcord-plugin-divider" />
-      { 
-        Object.keys(plugins.pluginCache.ghost).map(
-          plugin => {
-            return <PluginCard pluginId={plugin}/>;
-          }
-        )
-      }
-    </FormSection>
+    <ErrorBoundary>
+      <FormSection>
+        <FormTitle tag="h1">Plugins</FormTitle>
+        <Flex basis="auto" grow={1} shrink={1}>
+          <TextInput
+            className="cumcord-plugin-import"
+            placeholder="https://example.com/plugin"
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e)}
+          ></TextInput>
+          <Button
+            color={Button.Colors.BRAND}
+            size={Button.Sizes.MEDIUM}
+            onClick={() => {
+              setInput("");
+              plugins.importPlugin(input);
+            }}
+          >
+            Add plugin
+          </Button>
+        </Flex>
+        <FormDivider className="cumcord-plugin-divider" />
+        {Object.keys(plugins.pluginCache.ghost).map((plugin) => {
+          return <PluginCard pluginId={plugin} />;
+        })}
+      </FormSection>
+    </ErrorBoundary>
   );
 };
