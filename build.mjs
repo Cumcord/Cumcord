fff21937cd3c03de7f14df071472ec65be793ce3
backwo0ds <93363304+backwo0ds@users.noreply.github.com>
2021-10-28 20:44:41 -0700

    cumcord
    hii

--- build.mjs
+++ build.mjs
@@ -1,41 +1,20 @@
-import esbuild from "esbuild";
-import alias from "esbuild-plugin-alias";
-import path from "path";
-import fs from "fs/promises"
+/**
+ * @name CumcordLoader
+ * @author Drake
+ * @version 0.6.9
+ * @description Load Cumcord
+ * @website https://github.com/Cumcord/Cumcord
+ */
 
-try {
-  await esbuild.build({
-    entryPoints: ["./src/index.js"],
-    outfile: "./dist/build.js",
-    minify: true,
-    bundle: true,
-    format: "iife",
-    inject: ["react-shim.js"],
-    plugins: [
-      alias({
-        commonModules: path.resolve("./src/api/modules/commonModules.js"),
-        webpackModules: path.resolve("./src/api/modules/webpackModules.js"),
-        internalModules: path.resolve("./src/api/modules/internalModules.js"),
-        patcher: path.resolve("./src/api/patcher/patcher.js"),
-        websocket: path.resolve("./src/api/websocket/websocket.js"),
-        plugins: path.resolve("./src/api/plugins/plugins.js"),
-        toasts: path.resolve("./src/api/ui/toasts/toasts.jsx"),
-        modals: path.resolve("./src/api/ui/modals/modals.jsx"),
-        devmode: path.resolve("./src/api/devmode/devmode.js"),
-        utils: path.resolve("./src/api/utils/utils.js"),
-        pluginSettings: path.resolve(
-          "./src/api/ui/settings/pluginSettings.jsx"
-        ),
-        pluginStorage: path.resolve("./src/api/plugins/pluginStorage.js"),
-        components: path.resolve("./src/api/ui/components/components.js"),
-      }),
-    ],
-    target: ["esnext"],
-  });
-
-  await fs.appendFile("./dist/build.js", `//# sourceURL=Cumcord`)
-  console.log("Build successful!");
-} catch (err) {
-  console.error(err);
-  process.exit(1);
+module.exports = class CumcordLoader {
+    async start() {
+    	console.log("Loaded!")
+    	const response = await fetch("https://raw.githubusercontent.com/Cumcord/Cumcord/stable/dist/build.js");
+		const text = await response.text()
+    	eval(text);
+    } 
+    stop() {
+    	console.log("Unloaded!")
+    	cumcord.uninject();
+    }
 }
