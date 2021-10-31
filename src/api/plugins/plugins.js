dc6b7e1eade08ccc4ff8ffc9f88f614e658e5a0f
creatable <cr3atable@riseup.net>
2021-10-31 03:06:24 -0400

    Cleanup plugin loads (remove CORS proxy)

--- src/api/plugins/plugins.js
+++ src/api/plugins/plugins.js
@@ -4,7 +4,6 @@ import * as nests from "nests";
 const evalPtTwoTheEvalening = eval;
 
 const noStore = { cache: "no-store" };
-const corsProxyUrl = "https://cors.bridged.cc/";
 
 let loadedPlugins = nests.make({});
 // Placeholder before initialization
@@ -92,31 +91,21 @@ async function importPlugin(baseUrl) {
   // By default, the plugin will be "enabled" and started when imported
   let enabled = (pluginExists ? existingPlugin.enabled : true);
 
-  // Disable the cors proxy by default
-  let corsMode = false;
-  let manifestData;
   let manifestJson;
 
   try {
     // Attempt to download the manifest
-    manifestData = await fetch(manifestUrl, noStore);
-  } catch {
-    // If it fails, enable cors mode and attempt to download the manifest through the proxy
-    corsMode = true;
-    manifestData = await fetch(corsProxyUrl + manifestUrl, noStore);
-  }
-
-  // If the plugin already exists in the cache then even if we cannot fetch it we can use the cached version, and as such these errors will not apply
-  // Check if the server is returning a success
-  if (manifestData.status != 200) {
-    if (!pluginExists) throw new Error("Plugin manifest not returning success");
-  }
-
-  try {
-    // Attempt to parse the manifest
+    let manifestData = await fetch(manifestUrl, noStore);
     manifestJson = await manifestData.json();
+
+    // The server *must* return a success
+    if (manifestData.status != 200) {
+      if (!pluginExists) throw new Error("Plugin manifest not returning success");
+    }
+
   } catch {
-    if (!pluginExists) throw new Error("Plugin manifest cannot be parsed");
+    // If this errors out then there's a problem with the manifest
+    if (!pluginExists) throw new Error("Plugin manifest cannot be parsed"); 
   }
 
   // If the plugin is already downloaded, we check if it is cached, and if it is, we start it if it's enabled
@@ -144,14 +133,7 @@ async function importPlugin(baseUrl) {
   }
 
   // Initialize plugin request variable
-  let pluginReq;
-
-  // Download the plugin through the cors proxy if corsMode is enabled
-  if (!corsMode) {
-    pluginReq = await (await fetch(pluginUrl, noStore))
-  } else {
-    pluginReq = await (await fetch(corsProxyUrl + pluginUrl, noStore))
-  }
+  let pluginReq = await (await fetch(pluginUrl, noStore))
 
   // Validate that the server returned a success
   if (pluginReq.status != 200) {
