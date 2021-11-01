8d0a4340a7eaff3687844bc6a8e8dc85c8226455
creatable <cr3atable@riseup.net>
2021-11-01 05:11:30 -0400

    creatable is indecisive

--- src/api/ui/toasts/toasts.jsx
+++ src/api/ui/toasts/toasts.jsx
@@ -7,7 +7,7 @@ import toastStore from "./store.js";
 let toastDiv = document.createElement("div");
 
 function initializeToasts() {
-  injectCSS(`@keyframes cumcord-fadeIn{0%{opacity:0}100%{opacity:1}}.cumcord-toast-container{position:absolute;top:0;bottom:0;left:0;right:0;pointer-events:none;display:flex;flex-direction:column-reverse;align-items:flex-end;z-index:999999}.cumcord-toast{background-color:var(--background-floating);padding:18px;margin-right:10px;margin-bottom:10px;display:flex;flex-direction:column;justify-content:center;gap:14px;align-content:center;pointer-events:all;border-radius:5px;box-shadow:var(--elevation-high);animation:cumcord-fadeIn .3s;max-width:400px;overflow-wrap:anywhere}.cumcord-toast-title{font-size:30px;line-height:1}`);
+  injectCSS(`@keyframes cumcord-fadeIn{0%{opacity:0}100%{opacity:1}}.cumcord-toast-container{position:absolute;top:0;bottom:0;left:0;right:0;pointer-events:none;display:flex;flex-direction:column-reverse;align-items:flex-end;z-index:999999}.cumcord-toast{background-color:var(--background-floating);padding:18px;margin-right:10px;margin-bottom:10px;display:flex;flex-direction:column;justify-content:center;gap:14px;align-content:center;pointer-events:all;border-radius:5px;box-shadow:var(--elevation-high);animation:cumcord-fadeIn .3s;max-width:400px;overflow-wrap:anywhere}.cumcord-toast-title{font-size:22px;line-height:1}`);
   toastDiv.className = "cumcord-toast-container";
   document.getElementById("app-mount").prepend(toastDiv);
   ReactDOM.render(<ToastContainer />, toastDiv);
