3087c8b35aa5649c0aa35cad30a99bab402a3b47
creatable <cr3atable@riseup.net>
2021-11-05 04:10:32 -0400

    Add plugin card borders back

--- src/api/ui/settings/settings.js
+++ src/api/ui/settings/settings.js
@@ -5,7 +5,7 @@ import Plugins from "./components/Plugins.jsx";
 export default {
   initializeSettings: function () {
     // All patcher.injectCSS calls are uninjected when cumcord.uninject() is called, so this is never changed
-    injectCSS(`.cumcord-plugin-card{padding:16px;margin-bottom:10px;border-style: none}.cumcord-plugin-import{flex-grow:1;margin-right:20px}.cumcord-plugin-divider{margin-top:10px;margin-bottom:10px}.cumcord-plugin-search{margin-top:10px}.cumcord-card-header{display:inline-block}.cumcord-card-author,.cumcord-card-title{display:inline}.cumcord-card-right{display:flex}.cumcord-card-buttons{display:flex;margin-right:4px}.cumcord-card-buttons>*{fill:var(--interactive-normal);cursor:pointer;width:20px}.cumcord-card-buttons>*:hover{fill:var(--interactive-hover)}.cumcord-card-description{padding-top:3px;overflow-wrap:break-word}.cumcord-card-copy{width:18px}`);
+    injectCSS(`.cumcord-plugin-card{padding:16px;margin-bottom:10px}.cumcord-plugin-import{flex-grow:1;margin-right:20px}.cumcord-plugin-divider{margin-top:10px;margin-bottom:10px}.cumcord-plugin-search{margin-top:10px}.cumcord-card-header{display:inline-block}.cumcord-card-author,.cumcord-card-title{display:inline}.cumcord-card-right{display:flex}.cumcord-card-buttons{display:flex;margin-right:4px}.cumcord-card-buttons>*{fill:var(--interactive-normal);cursor:pointer;width:20px}.cumcord-card-buttons>*:hover{fill:var(--interactive-hover)}.cumcord-card-description{padding-top:3px;overflow-wrap:break-word}.cumcord-card-copy{width:18px}`);
 
     const Settings = webpackModules.findByDisplayName("SettingsView");
 
