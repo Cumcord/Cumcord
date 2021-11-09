d6e85a9b5437eba8d9fba98fea5b548bae12c769
creatable <cr3atable@riseup.net>
2021-11-09 15:44:51 -0500

    Prepare for big plugin API changes

--- src/api/plugins/plugins.js
+++ src/api/plugins/plugins.js
@@ -1,8 +1,6 @@
 import { createPersistentNest } from "pluginStorage";
 import * as nests from "nests";
 
-const evalPtTwoTheEvalening = eval;
-
 const noStore = { cache: "no-store" };
 
 let loadedPlugins = nests.make({});
@@ -11,7 +9,10 @@ let pluginCache = {};
 
 // This can be used to make other implementations of plugin loading (e.g dev mode)
 function evalPlugin(pluginCode, data) {
-  const pluginObject = evalPtTwoTheEvalening(pluginCode + `\n//# sourceURL=${data.id}`);
+  const pluginObject = new Function(
+    "__CUMCORD_PLUGIN_DATA",
+    "return " + pluginCode + `\n//# sourceURL=${data.id}`
+  )(data);
   let pluginData = pluginObject;
 
   if (typeof pluginObject == "function") {
@@ -33,7 +34,10 @@ async function startPlugin(pluginId) {
     throw new Error(`Plugin ${pluginId} already loaded`);
   }
 
-  const evaledPlugin = evalPlugin(plugin.js, { persist: await createPersistentNest(pluginId), id: pluginId });
+  const evaledPlugin = evalPlugin(plugin.js, {
+    persist: await createPersistentNest(pluginId),
+    id: pluginId,
+  });
 
   try {
     if (evaledPlugin["onLoad"]) {
@@ -78,10 +82,9 @@ function togglePlugin(pluginId) {
   }
 }
 
-
 async function importPlugin(baseUrl) {
   // Create standardized versions of the URL with a trailing / to prevent the ability to load plugins multiple times by removing a slash
-  const baseUrlTrailing = baseUrl.replace(/\/?$/, '/');
+  const baseUrlTrailing = baseUrl.replace(/\/?$/, "/");
   const manifestUrl = new URL("plugin.json", baseUrlTrailing);
   const pluginUrl = new URL("plugin.js", baseUrlTrailing);
 
@@ -90,10 +93,10 @@ async function importPlugin(baseUrl) {
 
   if (pluginExists) {
     existingPlugin = pluginCache.store[baseUrlTrailing];
-  }  
+  }
 
   // By default, the plugin will be "enabled" and started when imported
-  let enabled = (pluginExists ? existingPlugin.enabled : true);
+  let enabled = pluginExists ? existingPlugin.enabled : true;
 
   let manifestJson;
 
@@ -104,12 +107,12 @@ async function importPlugin(baseUrl) {
 
     // The server *must* return a success
     if (manifestData.status != 200) {
-      if (!pluginExists) throw new Error("Plugin manifest not returning success");
+      if (!pluginExists)
+        throw new Error("Plugin manifest not returning success");
     }
-
   } catch {
     // If this errors out then there's a problem with the manifest
-    if (!pluginExists) throw new Error("Plugin manifest cannot be parsed"); 
+    if (!pluginExists) throw new Error("Plugin manifest cannot be parsed");
   }
 
   // If the plugin is already downloaded, we check if it is cached, and if it is, we start it if it's enabled
@@ -122,14 +125,14 @@ async function importPlugin(baseUrl) {
         }
 
         if (enabled) {
-          startPlugin(baseUrlTrailing)
+          startPlugin(baseUrlTrailing);
         }
 
         return;
       } // The plugin will load if the server is unreachable
     } else {
       if (enabled) {
-        startPlugin(baseUrlTrailing)
+        startPlugin(baseUrlTrailing);
       }
 
       return;
@@ -137,7 +140,7 @@ async function importPlugin(baseUrl) {
   }
 
   // Initialize plugin request variable
-  let pluginReq = await (await fetch(pluginUrl, noStore))
+  let pluginReq = await await fetch(pluginUrl, noStore);
 
   // Validate that the server returned a success
   if (pluginReq.status != 200) {
@@ -151,7 +154,7 @@ async function importPlugin(baseUrl) {
   pluginCache.store[baseUrlTrailing] = {
     manifest: manifestJson,
     js,
-    enabled
+    enabled,
   };
 
   // Start it if it's enabled
@@ -181,7 +184,6 @@ async function initializePlugins() {
   }
 }
 
-
 function unloadAllPlugins() {
   for (let plugin of Object.keys(pluginCache.ghost)) {
     try {
@@ -201,5 +203,5 @@ export {
   initializePlugins,
   unloadAllPlugins,
   pluginCache,
-  loadedPlugins
-}
+  loadedPlugins,
+};
