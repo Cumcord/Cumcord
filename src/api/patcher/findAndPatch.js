163f36f854d03022e607b13c5bb952288424ab90
creatable <cr3atable@riseup.net>
2022-03-23 14:08:05 -0400

    fix findAndPatch not handling unpatching properly

--- src/api/patcher/findAndPatch.js
+++ src/api/patcher/findAndPatch.js
@@ -1,15 +1,18 @@
 import { findAsync } from "webpackModules"
 
 export default function (moduleFinder, patchCallback) {
+  let cancelled = false;
   let unpatch;
 
   const [modPromise, webpackUnpatch] = findAsync(moduleFinder, false)
 
   modPromise.then((mod) => {
+    if (cancelled) return;
     unpatch = patchCallback(mod)
   })
 
   return () => {
+    cancelled = true;
     webpackUnpatch?.()
     unpatch?.()
   }
