f81deecf8525a81576445909fbf743209376e6ae
Phorcys <57866459+phorcys420@users.noreply.github.com>
2022-05-23 20:55:55 +0200

    Use internal idb keyval instead of idb-keyval in plugin storage (for CumLoad data replication on web browser)

--- src/api/plugins/pluginStorage.js
+++ src/api/plugins/pluginStorage.js
@@ -1,12 +1,12 @@
 import * as nests from "nests";
-import { get, set } from "idb-keyval";
+import internalModules from "internalModules";
 
 // This function returns an object that will automatically be saved to indexeddb on modification
 async function createPersistentNest(prefix) {
-  const cached = (await get(`${prefix}_CUMCORD_STORE`)) || {};
+  const cached = (await internalModules.idbKeyval.get(`${prefix}_CUMCORD_STORE`)) || {};
   const nest = nests.make(cached);
 
-  let save = () => set(`${prefix}_CUMCORD_STORE`, { ...nest.ghost });
+  let save = () => internalModules.idbKeyval.set(`${prefix}_CUMCORD_STORE`, { ...nest.ghost });
 
   nest.on(nests.Events.SET, save);
   nest.on(nests.Events.DELETE, save);
