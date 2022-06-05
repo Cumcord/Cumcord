12f17b79d3af611c2fd5acc0678809684ebf9b9d
yellowsink <yellowsink@riseup.net>
2022-06-05 18:08:09 +0100

    format idk

--- build.mjs
+++ build.mjs
@@ -8,37 +8,39 @@ const jsconfig = JSON.parse(await fs.readFile("./jsconfig.json"));
 
 // converts jsconfig paths to esbuild aliases
 const aliases = Object.fromEntries(
-	Object.entries(jsconfig.compilerOptions.paths)
-		.map(([alias, [target]]) => [alias, path.resolve(target)])
+  Object.entries(jsconfig.compilerOptions.paths).map(([alias, [target]]) => [
+    alias,
+    path.resolve(target),
+  ]),
 );
 
 try {
-	await esbuild.build({
-		entryPoints: ["./src/index.js"],
-		outfile: "./dist/new-build.js",
-		minify: true,
-		bundle: true,
-		format: "iife",
+  await esbuild.build({
+    entryPoints: ["./src/index.js"],
+    outfile: "./dist/new-build.js",
+    minify: true,
+    bundle: true,
+    format: "iife",
     // injects the `React` global into our bundle (for JSX)
-		inject: ["./shims/global-react.js"],
-		external: ["react"],
-		plugins: [
-			{
+    inject: ["./shims/global-react.js"],
+    external: ["react"],
+    plugins: [
+      {
         // injects our React whenever "react" is imported
-				name: "external-react",
-				setup: (build) => 
-					build.onResolve({filter: /^react$/}, () => ({
-						path: path.resolve("./shims/import-react.cjs"),
-					})),
-			},
-			alias(aliases),
-		],
-		target: ["esnext"],
-	});
+        name: "external-react",
+        setup: (build) =>
+          build.onResolve({ filter: /^react$/ }, () => ({
+            path: path.resolve("./shims/import-react.cjs"),
+          })),
+      },
+      alias(aliases),
+    ],
+    target: ["esnext"],
+  });
 
-	await fs.appendFile("./dist/new-build.js", `//# sourceURL=Cumcord`);
-	console.log("Build successful!");
+  await fs.appendFile("./dist/new-build.js", `//# sourceURL=Cumcord`);
+  console.log("Build successful!");
 } catch (err) {
-	console.error(err);
-	process.exit(1);
+  console.error(err);
+  process.exit(1);
 }
