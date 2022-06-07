fa26948ae344bda3f7e103825122fdf7fc90dd88
yellowsink <yellowsink@riseup.net>
2022-06-07 22:01:07 +0100

    wire up current langs and hopefully not brick weblate again

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -2,10 +2,24 @@ import { i18n } from "@commonModules";
 
 import fallback from "./default.json";
 
-import enGB from "./en-GB.json";
+import gb from "./en-GB.json";
+import de from "./de.json";
+import fr from "./fr.json";
+import hu from "./hu.json";
+import it from "./it.json";
+import nl from "./nl.json";
+import pl from "./pl.json";
+import ru from "./ru.json";
 
 const langs = {
-  "en-GB": enGB,
+  "en-GB": gb,
+  de,
+  fr,
+  hu,
+  it,
+  nl,
+  pl,
+  ru,
 };
 
 const consts = new Proxy(fallback, {
