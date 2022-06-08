ae64df45fed81073571e7021627ce11988166ddf
yellowsink <yellowsink@riseup.net>
2022-06-08 17:19:36 +0100

    hook up some more langs :)

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -2,24 +2,32 @@ import { i18n } from "@commonModules";
 
 import fallback from "./default.json";
 
-import gb from "./en-GB.json";
 import de from "./de.json";
+import el from "./el.json";
+import gb from "./en-GB.json";
 import fr from "./fr.json";
+import hi from "./hi.json";
 import hu from "./hu.json";
 import it from "./it.json";
 import nl from "./nl.json";
 import pl from "./pl.json";
 import ru from "./ru.json";
+import tr from "./tr.json";
+import vi from "./vi.json";
 
 const langs = {
-  "en-GB": gb,
   de,
+  el,
+  "en-GB": gb,
   fr,
+  hi,
   hu,
   it,
   nl,
   pl,
   ru,
+  tr,
+  vi,
 };
 
 const consts = new Proxy(fallback, {
